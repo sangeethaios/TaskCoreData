2cc462dc6f23d4ec15248e4cad3cc3c5ac16ae66
sangeethaios <sangeethag1207@gmail.com>
2024-04-29 15:40:27 +0530

    Initial Commit

---
+++ TaskCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskCoreData
+//
+//  Created by SangeethaKalis on 29/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
